00d1194d82b19a6baa8f12d581ba19511075aec2
srinath-03937 <srinath-03937@users.noreply.github.com>
2026-03-11 19:07:14 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 // Initialize Firebase
 const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n').replace(/"/g, ''),
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   databaseURL: process.env.FIREBASE_DATABASE_URL
 };
